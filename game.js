c536ebc1317832653cf8bdb9c2a893d072637dbc
HarryCandlish <hcnd90@gmail.com>
2019-06-15 10:57:08 +1200

    calculate the sum of all frames

--- game.js
+++ game.js
@@ -1,7 +1,18 @@
 // Score 119:
 // var frames = [
-//   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
-// ]
+//   [
+//     [1, 2],
+//     [6, 4],
+//     [5, 4],
+//     [10, 0],
+//     [7, 2],
+//     [10, 0],
+//     [10, 0],
+//     [5, 2],
+//     [7, 0],
+//     [4, 4]
+//   ]
+// ];
 //
 // Score 141:
 // var frames = [
@@ -17,6 +28,16 @@ function scoreFrame(frame) {
   return frame[0] + frame[1];
 }
 
+function sumOfFrames(frames) {
+  return frames.reduce((acc, frame, i) => {
+    // accumulator = adds the contents of each array together
+    // frame(current value) = the contents of each individual array within the array
+    // i (current index)= index of each array within the array
+    return acc + scoreFrame(frame);
+  }, 0);
+}
+
 module.exports = {
-  scoreFrame
+  scoreFrame,
+  sumOfFrames
 };
--- tests/tests.test.js
+++ tests/tests.test.js
@@ -1,7 +1,26 @@
 var game = require("../game");
+
 test("scoreFrame", () => {
   var frame = [1, 2];
   var expected = 3;
   var actual = game.scoreFrame(frame);
+  expect(actual).toBe(expected);
+});
+
+test("add all frames", () => {
+  var frames = [
+    [1, 2],
+    [6, 4],
+    [5, 4],
+    [10, 0],
+    [7, 2],
+    [10, 0],
+    [10, 0],
+    [5, 2],
+    [7, 0],
+    [4, 4]
+  ];
+  var expected = 83;
+  var actual = game.sumOfFrames(frames);
   expect(actual).toBe(expected);
 });
